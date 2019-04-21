8a065ad90d1e40d0ce7f064ad422ad8034f8f62f
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-21 12:34:09 +0200

    final solution

---
+++ Dynamic_Memory_Management_Stack_Using_Array/Pila.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dynamic_Memory_Management_Stack_Using_Array
+{
+    class Pila
+    {
+        int valor_actual;
+        int cantidad_valores;
+        int[] valores_pila;
+
+        public Pila(int cantidad_valores)
+        {
+            valor_actual = 0;
+            this.cantidad_valores = cantidad_valores;
+
+            valores_pila = new int[cantidad_valores];
+        }
+
+        public void Apilar(int valor)
+        {
+            if (valor_actual < cantidad_valores)
+            {
+                valores_pila[valor_actual] = valor;
+                valor_actual++;
+            }
+
+        }
+
+        public int Desapilar()
+        {
+            if (valor_actual > 0)
+            {
+                valor_actual -= 1;
+                return valores_pila[valor_actual];
+            }
+            return 0;
+        }
+    }
+}
--- Dynamic_Memory_Management_Stack_Using_Array/Program.cs
+++ Dynamic_Memory_Management_Stack_Using_Array/Program.cs
@@ -6,7 +6,20 @@ namespace Dynamic_Memory_Management_Stack_Using_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Implementing a stack using array
+
+            bool depurando = true;
+
+            Pila miPila = new Pila(5);
+
+            miPila.Apilar(4);
+            miPila.Apilar(5);
+
+            Console.WriteLine(miPila.Desapilar());
+            Console.WriteLine(miPila.Desapilar());
+
+            if (depurando)
+                Console.ReadLine();
         }
     }
 }
